95f108b587c92e9407e680b23ca62b33fba5f608
miteymouse <chenghtmark@gmail.com>
2014-03-11 15:42:42 +0800

    Set up Rails, fixed errors

---
+++ app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App::Application.config.secret_key_base = 'a052b08c868fc954ba42ec3031ec2a90e8810ad9d05c0b9bb679f68a29f969bdf90311967df930a15fe7e54e139bebc8f968af53156ca4809ff330e7d4fc8a7a'
---
+++ app/config/routes.rb
@@ -0,0 +1,3 @@
+App::Application.routes.draw do
+  root 'site#index'
+end
